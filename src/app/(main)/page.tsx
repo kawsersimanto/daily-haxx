896b5105251b495cbbc2b992417854ec1d1d7934
kawserimanto <kawsersimanto@gmail.com>
2025-07-14 09:41:56 +0600

    fixed theme hydration error for light and dark mode theme

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,14 @@
+const Main = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div>
+      asdf
+      {children}
+    </div>
+  );
+};
+
+export default Main;
--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import * as React from "react";
 
 export function ThemeProvider({
   children,
-  ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
