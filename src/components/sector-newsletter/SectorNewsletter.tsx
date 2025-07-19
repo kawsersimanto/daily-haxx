dd9b9743a7f8c59beaf5698c032d2940bff05342
Kawser Simanto <kawsersimanto@gmail.com>
2025-07-19 21:04:56 +0600

    updated section padding

--- src/components/sector-newsletter/SectorNewsletter.tsx
+++ src/components/sector-newsletter/SectorNewsletter.tsx
@@ -6,7 +6,7 @@ const SectorNewsletter = () => {
   return (
     <div>
       <SectionTitle>Sector Newsletter</SectionTitle>
-      <div className="grid md:grid-cols-4 grid-cols-1 gap-3.5 mb-15">
+      <div className="grid md:grid-cols-4 grid-cols-1 gap-3.5 md:mb-15 mb-10">
         {news.slice(8, 16).map((news, index) => (
           <NewsCard key={index} data={news} />
         ))}
