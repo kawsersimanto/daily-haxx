94d35681d157a5f728d4a13563348009f2389cb0
Kawser Simanto <kawsersimanto@gmail.com>
2025-07-20 11:33:12 +0600

    intialized zustand

---
+++ src/store/auth/authStore.ts
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
+import { createSelectors } from "../selector";
+
+type AuthState = {
+  token: string | null;
+  setToken: (token: string) => void;
+  logout: () => void;
+};
+
+const authStore = create<AuthState>()(
+  persist(
+    immer((set) => ({
+      token: null,
+      setToken: (token: string) =>
+        set((state) => {
+          state.token = token;
+        }),
+      logout: () =>
+        set((state) => {
+          state.token = null;
+        }),
+    })),
+    {
+      name: "auth",
+      partialize: (state) => ({ token: state.token }),
+    }
+  )
+);
+
+export const useAuthStore = createSelectors(authStore);
