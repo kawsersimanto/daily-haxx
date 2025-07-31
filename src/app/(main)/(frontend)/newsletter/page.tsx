7451384d8b3d91e67e2c1a031fb8b6ea102d6c62
kawserimanto <kawsersimanto@gmail.com>
2025-07-31 20:08:21 +0600

    initializzed newsletter page

--- src/app/(main)/(frontend)/newsletter/page.tsx
+++ src/app/(main)/(frontend)/newsletter/page.tsx
@@ -1,5 +1,9 @@
 const NewsletterPage = () => {
-  return <div>NewsletterPage</div>;
+  return (
+    <section className="pt-[60px] pb-[160px]">
+      <div className="container">hasd</div>
+    </section>
+  );
 };
 
 export default NewsletterPage;
