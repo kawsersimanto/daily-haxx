eba5374163bc6b728891fea233494ceca3d4c017
Kawser Simanto <kawsersimanto@gmail.com>
2025-08-12 01:09:53 +0600

    added global api response type

--- src/features/newsletter/types.ts
+++ src/features/newsletter/types.ts
@@ -2,3 +2,11 @@ export interface Newsletter {
   id: string;
   // define fields
 }
+
+export interface NewsletterQueryParams {
+  page: number;
+  limit: number;
+  total: number;
+  search: string;
+  filter: string;
+}
