f32d23e2a7a9a4da3824a4e2322af9d0b9c67931
Kawser Simanto <kawsersimanto@gmail.com>
2025-08-13 00:26:57 +0600

    newsletter details page

--- src/app/(main)/(frontend)/newsletter/[slug]/page.tsx
+++ src/app/(main)/(frontend)/newsletter/[slug]/page.tsx
@@ -1,5 +1,44 @@
-const NewsletterDetails = () => {
-  return <section className="pt-12 pb-20">NewsletterDetails</section>;
+import { Logo } from "@/shared";
+
+const NewsletterDetails = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  console.log((await params).slug);
+
+  return (
+    <section className="md:pt-12 pt-7 md:pb-20 pb-10 px-5">
+      <div className="max-w-[793px] mx-auto">
+        <div className="flex flex-col md:gap-12.5 gap-5">
+          <Logo className="mx-auto xl:w-[200px] w-[140px] h-full" />
+          <div className="md:py-7.5 py-3 md:px-10 px-5 bg-card border border-border rounded-xl prose md:text-lg text-sm">
+            <h2>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi,
+              fuga!
+            </h2>
+            <h3>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum
+              explicabo iure qui fugiat enim accusamus sapiente ullam nemo fugit
+              assumenda unde impedit aliquam nam, corporis earum at dolores
+              aspernatur quos.
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Doloremque odio quod commodi incidunt maiores, ipsum maxime
+              adipisci nisi natus illum vero saepe aperiam animi! Sequi fugit
+              consequuntur eos sed hic!
+            </p>
+            <ol>
+              <li>lorem10</li>
+              <li>lorem10</li>
+              <li>lorem10</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default NewsletterDetails;
