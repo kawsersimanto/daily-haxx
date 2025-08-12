009ac1ee6479539f4b7b414daa5a979b2b1d62c5
Kawser Simanto <kawsersimanto@gmail.com>
2025-08-13 03:29:45 +0600

    Added section title to article page

--- src/app/(main)/(frontend)/article/page.tsx
+++ src/app/(main)/(frontend)/article/page.tsx
@@ -1,5 +1,18 @@
+import { SectionHeader } from "@/components/section-header/SectionHeader";
+
 const ArticlePage = () => {
-  return <div>ArticlePage</div>;
+  return (
+    <section className="pt-15 md:pb-40 pb-20">
+      <div className="container">
+        <div>
+          <SectionHeader title="Articles">
+            Read our previous insight-driven newsletters covering business,
+            finance, economics and tech.
+          </SectionHeader>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ArticlePage;
