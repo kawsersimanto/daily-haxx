e9f608b6f6c45881fb04928f1816ebb3d0d0780d
Kawser Simanto <kawsersimanto@gmail.com>
2025-08-12 19:05:34 +0600

    created pagination component for reusability

--- src/features/newsletter/components/NewsletterList.tsx
+++ src/features/newsletter/components/NewsletterList.tsx
@@ -1,11 +1,26 @@
-import { NewsletterCard, newsletters } from "@/features/newsletter";
+"use client";
+
+import { Pagination } from "@/components/pagination/Pagination";
+import {
+  NewsletterCard,
+  newsletters,
+  useNewsletterSelector,
+} from "@/features/newsletter";
 
 export const NewsletterList = () => {
+  const page = useNewsletterSelector.use.page();
+  const setPage = useNewsletterSelector.use.setPage();
+  const totalPages = 20;
   return (
     <div className="flex flex-col md:gap-6 gap-4">
       {newsletters.map((newsletter) => (
         <NewsletterCard key={newsletter?.id} data={newsletter} />
       ))}
+      <Pagination
+        currentPage={page}
+        totalPages={totalPages}
+        onPageChange={setPage}
+      />
     </div>
   );
 };
