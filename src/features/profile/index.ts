5624ce7798e8448ddd6885d84486be6eebfdfe5a
Kawser Simanto <kawsersimanto@gmail.com>
2025-08-16 02:48:07 +0600

    fixed build errors

--- src/features/profile/index.ts
+++ src/features/profile/index.ts
@@ -2,7 +2,6 @@ export * from "./api";
 export * from "./components";
 export * from "./constants";
 export * from "./hooks";
-export * from "./mocks/profile.mock";
 export * from "./schema/profileSchema";
 export * from "./store/profileStore";
 export * from "./types";
